84476163e4b08759702954caa05351a0d59e7d0c
Devante Lowery <dlowery2120@gmail.com>
2021-02-15 04:39:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,5 +27,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find {|n| n == valid_calls}
+  return n
   
 end
